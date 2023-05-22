49df129ddbe59b4b73b0bfb3c9def687a13c12b9
djdoolky76 <30578879+djdoolky76@users.noreply.github.com>
2023-05-23 02:01:25 +0800

    translate some logs to english

--- internal/core/console.go
+++ internal/core/console.go
@@ -15,14 +15,14 @@ import (
 
 const (
 	consoleUid         = uint32(1)
-	consoleNickname    = "望星忆君"
+	consoleNickname    = "djdoolky76"
 	consoleLevel       = uint32(60)
 	consoleWorldLevel  = uint32(8)
-	consoleSignature   = ""
+	consoleSignature   = "Welcome to GIO Server"
 	consoleNameCardId  = uint32(210001)
 	consoleAvatarId    = uint32(10000077)
 	consoleCostumeId   = uint32(0)
-	consoleWelcomeText = "望星开发服 gio 3.4 dev \n进度缓慢，推荐出门左转前往桜开发服"
+	consoleWelcomeText = "Welcome to ViaGenshin Dev \nThis is experimental server, Major issues may arise."
 )
 
 type MuipResponseBody struct {
@@ -36,7 +36,7 @@ type MuipResponseBody struct {
 }
 
 func (s *Server) ConsoleExecute(cmd, uid uint32, text string) (string, error) {
-	logger.Info().Uint32("uid", uid).Msgf("控制台执行: %s", text)
+	logger.Info().Uint32("uid", uid).Msgf("Console Execution: %s", text)
 	var values []string
 	values = append(values, fmt.Sprintf("cmd=%d", cmd))
 	values = append(values, fmt.Sprintf("uid=%d", uid))
@@ -44,7 +44,7 @@ func (s *Server) ConsoleExecute(cmd, uid uint32, text string) (string, error) {
 	values = append(values, fmt.Sprintf("region=%s", s.config.Console.MuipRegion))
 	ticket := make([]byte, 16)
 	if _, err := rand.Read(ticket); err != nil {
-		return "", fmt.Errorf("无法生成 ticket: %w", err)
+		return "", fmt.Errorf("Unable to process your ticket: %w", err)
 	}
 	values = append(values, fmt.Sprintf("ticket=%x", ticket))
 	if s.config.Console.MuipSign != "" {
@@ -54,29 +54,29 @@ func (s *Server) ConsoleExecute(cmd, uid uint32, text string) (string, error) {
 		values = append(values, fmt.Sprintf("sign=%x", shaSum.Sum(nil)))
 	}
 	uri := s.config.Console.MuipEndpoint + "?" + strings.ReplaceAll(strings.Join(values, "&"), " ", "+")
-	logger.Debug().Msgf("Muip 响应: %s", uri)
+	logger.Debug().Msgf("MUIP Response: %s", uri)
 	resp, err := http.Get(uri)
 	if err != nil {
-		return "Muip 响应: %s" + "\n温馨提示" + consoleWelcomeText, err
+		return "Muip Response: %s" + "\nWarn reminder that the sending options may be limited." + consoleWelcomeText, err
 	}
 	defer resp.Body.Close()
 	p, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
-	logger.Debug().Msgf("Muip 响应: %s", string(p))
+	logger.Debug().Msgf("MUIP Response: %s", string(p))
 	if resp.StatusCode != 200 {
-		return "Muip 响应: %s" + "\n温馨提示" + consoleWelcomeText, fmt.Errorf("状态码: %d", resp.StatusCode)
+		return "MUIP Response: %s" + "\nWarn reminder that the sending options may be limited." + consoleWelcomeText, fmt.Errorf("Status Code: %d", resp.StatusCode)
 	}
 	body := new(MuipResponseBody)
 	if err := json.Unmarshal(p, body); err != nil {
 		return "", err
 	}
 	if (text == "help") {
-		return "gm指令往此输入", nil
+		return "To execute command, type gm command here.", nil
 	}
 	if body.Retcode != 0 {
-		return "执行命令失败: " + body.Data.Msg + ", 错误: " + body.Msg + "\n温馨提示" + consoleWelcomeText, nil
+		return "Failed to execute command: " + body.Data.Msg + ", Make sure keyword command is correct: " + body.Msg + "\nTips+ consoleWelcomeText, nil
 	}
-	return "执行命令成功: " + body.Data.Msg + "\n温馨提示" + consoleWelcomeText, nil
+	return "The command was executed successfully: " + body.Data.Msg + "\nMsg" + consoleWelcomeText, nil
 }
